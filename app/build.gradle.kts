d8c8e5cdc611fa585cc4d4498335d74dcf826f23
IMaX23432 <imaxmatummai2233443322@gmail.com>
2026-01-27 16:24:25 +0700

    HP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.a508lablearnandroid"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.a508lablearnandroid"
-        minSdk = 29
-        targetSdk = 35
+        minSdk = 30
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
